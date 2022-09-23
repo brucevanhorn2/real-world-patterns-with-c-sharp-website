02762ef6229f98bc0017687717cf600569ce048b
Bruce Van Horn <brucevanhorn@me.com>
2022-09-23 17:19:57 -0500

    prettier run

--- js/site.js
+++ js/site.js
@@ -1,14 +1,14 @@
-$( document ).ready(function() {
-    // button handler for the modal
-    $("#btnNotify").click(function(){
-        $(".modal").addClass("is-active");
-    });
+$(document).ready(function () {
+  // button handler for the modal
+  $("#btnNotify").click(function () {
+    $(".modal").addClass("is-active");
+  });
 
-    $(".modal-close").click(function(){
-        $(".modal").removeClass("is-active");
-    });
+  $(".modal-close").click(function () {
+    $(".modal").removeClass("is-active");
+  });
 
-    $(".modal-background").click(function(){
-        $(".modal").removeClass("is-active");
-    });
+  $(".modal-background").click(function () {
+    $(".modal").removeClass("is-active");
+  });
 });

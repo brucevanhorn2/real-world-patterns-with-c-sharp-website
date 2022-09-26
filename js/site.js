8ea151cab918589c8de7602957ae49790e44206a
Bruce Van Horn <brucevanhorn@me.com>
2022-09-26 16:50:02 -0500

    moving signup to its own page

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   // button handler for the modal
-  $("#btnNotify").click(function () {
-    $(".modal").addClass("is-active");
-  });
+  // $("#btnNotify").click(function () {
+  //   $(".modal").addClass("is-active");
+  // });
 
-  $(".modal-close").click(function () {
-    $(".modal").removeClass("is-active");
-  });
+  // $(".modal-close").click(function () {
+  //   $(".modal").removeClass("is-active");
+  // });
 
-  $(".modal-background").click(function () {
-    $(".modal").removeClass("is-active");
-  });
+  // $(".modal-background").click(function () {
+  //   $(".modal").removeClass("is-active");
+  // });
   // handler for the nav burger
   $(".navbar-burger").click(function() {
     $(".navbar-burger").toggleClass("is-active");

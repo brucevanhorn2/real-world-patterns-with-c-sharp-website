1c6e913f7fb58160f6383d804c76dd06e1564be0
Bruce Van Horn <brucevanhorn@me.com>
2022-09-26 17:25:50 -0500

    added lazy loaded images and a better font tothe signup header

--- js/site.js
+++ js/site.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-  // button handler for the modal
-  // $("#btnNotify").click(function () {
-  //   $(".modal").addClass("is-active");
-  // });
-
-  // $(".modal-close").click(function () {
-  //   $(".modal").removeClass("is-active");
-  // });
-
-  // $(".modal-background").click(function () {
-  //   $(".modal").removeClass("is-active");
-  // });
   // handler for the nav burger
   $(".navbar-burger").click(function() {
     $(".navbar-burger").toggleClass("is-active");

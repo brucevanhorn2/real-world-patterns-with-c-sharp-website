75361bf5b67ccdb98ee90c343de09febf8f68682
Bruce Van Horn <brucevanhorn@me.com>
2022-09-29 22:38:29 -0500

    added countdown timer

--- js/site.js
+++ js/site.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
     $(".navbar-menu").toggleClass("is-active");
 
   });
+
+  const counterElement = document.querySelector("#counter");
+  new ysCountDown(endDate, function (remaining, finished) {
+      var message = "";
+      if (finished) {
+        message = "Expired";
+      } else {
+        message = remaining.totalDays + "d ";
+        message += (remaining.hours < 10 ? '0' + remaining.hours : remaining.hours)  + "h ";
+        message += (remaining.minutes < 10 ? '0' + remaining.minutes : remaining.minutes)  + "m ";
+        message += (remaining.seconds <10 ? '0' + remaining.seconds : remaining.seconds) + "s ";
+      }
+      counterElement.textContent = message;
+  });
 });
+const endDate = "2022/10/14";
